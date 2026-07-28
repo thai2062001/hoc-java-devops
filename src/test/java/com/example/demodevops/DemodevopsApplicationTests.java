17e6ecd98222d30092aa7f8a94a1be126ff148f4
thailanhat7-del <thailanhat7@gmail.com>
2026-07-28 07:33:42 +0000

    Update

--- src/test/java/com/example/demodevops/DemodevopsApplicationTests.java
+++ src/test/java/com/example/demodevops/DemodevopsApplicationTests.java
@@ -1,19 +1,21 @@
 package com.example.demodevops;
 
+import com.example.demodevops.controller.MemberController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class DemodevopsApplicationTests {
 
     @Autowired
-    private ApiController apiController;
+    private MemberController memberController;
 
     @Test
     void testGetAllMembersNotNull() {
-        // Bài test quy định: Hàm này chạy PHẢI ra kết quả, KHÔNG ĐƯỢC PHÉP NULL!
-        assertNotNull(apiController.getAllMembers(), "Lỗi nghiêm trọng: Danh sách trả về bị null rồi!");
+        // Kiểm tra xem MemberController và hàm getAllMembers() mới hoạt động ngon lành, không bị NULL!
+        assertNotNull(memberController.getAllMembers(), "Lỗi nghiêm trọng: Danh sách trả về bị null rồi!");
     }
 }
